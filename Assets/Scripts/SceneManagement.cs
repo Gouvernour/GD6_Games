555ea8c58f10a891738b5d3201187fcab0351453
Amanda Östman <amanda-ostman@hotmail.com>
2023-01-27 21:24:40 +0100

    start menu done

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    [SerializeField] string daSceneName;
+    bool timerStart = false;
+    public float timerTime = 2.0f;
+    public GameObject pressAny;
+
+    void Update()
+    {
+        if (Input.anyKey && !timerStart)
+        {
+            StartCoroutine(timer());
+            timerStart = true;
+        }
+    }
     public void StartGame(string sceneName)
     {
+
         SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator timer ()
+    {
+        pressAny.GetComponent<Animator>().Play("Start");
+       yield return new WaitForSeconds(timerTime);
+        StartGame(daSceneName);
     }
 }
